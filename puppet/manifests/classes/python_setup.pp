1d4d13034d8cf9706ec56c51d130b58aaa4a04b0
Gavin Noronha <gavinln@hotmail.com>
2015-11-09 22:52:21 -0800

    added tornado library for ipython

--- puppet/manifests/classes/python_setup.pp
+++ puppet/manifests/classes/python_setup.pp
@@ -29,10 +29,15 @@ class python_setup {
                 provider => pip,
                 require => Package['python-pip']
             }
+            package { 'tornado':
+                ensure => installed,
+                provider => pip,
+                require => Package['python-pip']
+            }
             package { 'ipython':
                 ensure => '3.2.1',
                 provider => pip,
-                require => Package['python-zmq', 'jinja2']
+                require => Package['python-zmq', 'jinja2', 'tornado']
             }
             # pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
         }
